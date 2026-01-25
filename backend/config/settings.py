cdc3e4e6a7e7cec41365af7aaa7070c0bd442875
NoahKimutai <noah.kimutai@ledr.io>
2026-01-25 15:54:55 +0300

    update backend dockerfile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,9 +99,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default="postgresql://postgres:postgres@localhost:5432/trip-planner",
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
+        ssl_require=not os.environ.get("DEBUG", False),
     )
 }
 
